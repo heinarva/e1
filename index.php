dccf2860a3984d4a1b6d736c0b22d67fde70909a
heinarva <heinarva@netscape.net>
2015-02-23 21:48:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,51 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="et-ee" lang="et-ee" dir="ltr" >
+
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>-E1-</title>
+
+
 <?php
+$K = '';
+//!!jquery-1.9.1.js kaib IE-ga
+//$K  .= '<script type=\'text/javascript\' src=\'//code.jquery.com/jquery-1.9.1.js\'></script>';
+$K  .= '<script type=\'text/javascript\' src=\'//code.jquery.com/jquery-1.10.2.js\'></script>';
+$K  .= '<script type=\'text/javascript\' src=\'//code.jquery.com/ui/1.10.4/jquery-ui.js\'></script>';
+
+$K  .= '<script src=\'js/master1.js\'  type=\'text/javascript\'></script>';
+//	$K  .= '<script src=\'js/master.js\'  type=\'text/javascript\'></script>';
+
+//http://www.w3schools.com/css/css_table.asp
+//http://www.csstutorial.net/css_tutorial_part3.php
+//http://matthewjamestaylor.com/blog/adding-css-to-html-with-link-embed-inline-and-import
+$K  .= '<link rel="stylesheet" type="text/css" href="css/tbl1.css" media="screen" />';
+
+echo $K;
 
 ?>
+
+ </head>
+ 
+<?php
+
+$K = '';
+$K .= '<body>';
+$K .= '<span id="pageBody">';
+$K .= ' - . - <br />';
+$K .= '</span>';
+$K .= '</body>';
+
+echo $K;
+/*
+ * https://amca01.wordpress.com/
+ * */
+?>
+ 
+ 
+</html>
+ 
